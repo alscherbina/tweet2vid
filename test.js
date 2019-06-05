aaa31a3a9e899ef135361d486366fd9e2114f2f8
Oleksandr Shcherbyna <1928072+alscherbina@users.noreply.github.com>
2019-06-05 23:01:45 +0300

    Refactored the code

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 const needle = require('needle');
 const fs = require('fs');
 const parseM3u8 = require('parse-m3u8');
+const stream = require('stream');
+const util = require('util');
+
+const pipeline = util.promisify(stream.pipeline);
+
 require('dotenv-safe').config();
 
 const videoDir = process.env.VIDEO_DIR;
@@ -22,29 +27,52 @@ async function getGuestToken() {
   return response.body.guest_token;
 }
 
-async function downloadVideo(postURL = 'https://twitter.com/pullover_/status/1135832570669797376') {
+async function getTweetConfig(tweetId) {
   const guestToken = await getGuestToken();
-  const tweetId = postURL.slice(postURL.lastIndexOf('/'));
   const tweetConfigURL = `https://api.twitter.com/1.1/videos/tweet/config/${tweetId}.json`;
   const headers = {
     ...defaultHeaders,
     'x-guest-token': guestToken
   };
   const response = await needle('get', tweetConfigURL, {}, { headers });
-
-  console.log(response.body.track.playbackUrl);
-  console.log(response.body.posterImage);
-  needle.get(response.body.posterImage).pipe(fs.createWriteStream(`${videoDir}/${tweetId}.jpg`));
-  if (response.body.track.playbackUrl.includes('.m3u8')) {
-    const fileName = `${videoDir}/${tweetId}.m3u8`;
-    const w = needle.get(response.body.track.playbackUrl).pipe(fs.createWriteStream(fileName));
-    w.on('finish', () => {
-      const contents = fs.readFileSync(fileName, 'utf8');
-      console.log(parseM3u8(contents));
-    });
-  } else {
-    needle.get(response.body.track.playbackUrl).pipe(fs.createWriteStream(`${videoDir}/${tweetId}.mp4`));
-  }
+  return response;
 }
 
-downloadVideo(process.argv[2]);
+async function downloadM3u8(tweetId, uri) {
+  const fileName = `${videoDir}/${tweetId}.m3u8`;
+
+  const response = await needle('get', uri);
+  const m3u8 = parseM3u8(response.body.toString('utf8'));
+  console.log(m3u8);
+}
+
+async function downloadMp4(tweetId, uri) {
+  needle.get(uri).pipe(fs.createWriteStream(`${videoDir}/${tweetId}.mp4`));
+}
+
+async function downloadVideo(postURL = 'https://twitter.com/pullover_/status/1135832570669797376') {
+  const tweetId = postURL.slice(postURL.lastIndexOf('/'));
+  const result = {
+    videoFile: `${videoDir}/${tweetId}.mp4`,
+    thumbnailFile: `${videoDir}/${tweetId}.jpg`
+  };
+  const tweetConfig = await getTweetConfig(tweetId);
+  const posterImageUrl = tweetConfig.body.posterImage;
+  const { playbackUrl } = tweetConfig.body.track;
+  result.thumbnailFile = `${videoDir}/${tweetId}.jpg`;
+
+  const imageDownloadPromise = pipeline(needle.get(posterImageUrl), fs.createWriteStream(result.thumbnailFile));
+  let videoDownloadPromise = Promise.resolve();
+  if (playbackUrl.includes('.m3u8')) {
+    videoDownloadPromise = downloadM3u8(tweetId, playbackUrl);
+  } else {
+    videoDownloadPromise = downloadMp4(playbackUrl);
+  }
+  await Promise.all([imageDownloadPromise, videoDownloadPromise]);
+  return result;
+}
+
+(async () => {
+  const res = await downloadVideo(process.argv[2]);
+  console.log(res);
+})().catch(error => console.error(error));
