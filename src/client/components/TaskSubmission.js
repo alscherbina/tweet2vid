4d5340a5622c59b654b425090711c78fc863cef4
Oleksandr_Shcherbyna <Oleksandr_Shcherbyna@epam.com>
2019-06-14 16:02:42 +0300

    Draft task submission component

--- src/client/components/TaskSubmission.js
+++ src/client/components/TaskSubmission.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from '../utils/axios';
 
 export default function TaskSubmission() {
-  return <div>Task submission</div>;
+  const [taskUrl, setTaskUrl] = useState('');
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  const onTaskInputChange = e => {
+    setTaskUrl(e.target.value);
+  };
+
+  const submitTask = async () => {
+    setSubmitting(true);
+    await axios.post('/videos/task', { twitterPostURL: taskUrl });
+    setSubmitting(false);
+  };
+
+  return (
+    <div>
+      <form>
+        <input name="url" type="text" value={taskUrl} onChange={onTaskInputChange} />
+        <button type="button" value="Load" onClick={submitTask} disabled={isSubmitting}>
+          Load video
+        </button>
+      </form>
+    </div>
+  );
 }
