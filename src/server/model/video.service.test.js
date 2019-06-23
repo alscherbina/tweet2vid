1dfce48717b7fa5ec9afed0e1c1d01d0f52aa0c4
Oleksandr Shcherbyna <1928072+alscherbina@users.noreply.github.com>
2019-06-23 21:25:46 +0300

    Added unit test for media removal service

---
+++ src/server/model/video.service.test.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const videoService = require('./video.service');
+
+const logger = require('../configs/logger');
+
+jest.mock('dotenv-safe');
+jest.mock('fs');
+jest.mock('../configs/logger', () => {
+  return {
+    info: jest.fn(() => 42)
+  };
+});
+
+fs.promises = {
+  unlink: jest.fn()
+};
+
+describe('Delete media', () => {
+  afterEach(() => {
+    logger.info.mockClear();
+    fs.promises.unlink.mockReset();
+  });
+
+  test('Delete media with invalid ID should throw error', async () => {
+    await expect(videoService.deleteMedia('1232142@#$')).rejects.toThrow();
+  });
+
+  test('Delete media with invalid ID should log the attempt', async () => {
+    try {
+      await videoService.deleteMedia('1232142@#$');
+    } catch (err) {}
+    expect(logger.info.mock.calls.length).toBe(1);
+  });
+
+  test('Delete media with valid ID should delete mp4 and jpg files via fs.promises.unlink', async () => {
+    await expect(videoService.deleteMedia('1232142')).resolves;
+    expect(fs.promises.unlink.mock.calls.length).toBe(2);
+  });
+
+  test('Delete media with valid ID should throw error in case of fs issues', async () => {
+    fs.promises.unlink.mockReturnValue(Promise.reject(new Error('Err')));
+    await expect(videoService.deleteMedia('1232142')).rejects.toThrow();
+  });
+});
